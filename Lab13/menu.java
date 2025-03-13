b7506fea165a27f3256ba46e7e119d9a529290d8
Harley2zaza <67070259@kmitl.ac.th>
2025-03-13 20:30:57 +0700

    Lab13

---
+++ Lab13/menu.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab13;
+
+/**
+ *
+ * @author USER
+ */
+public class menu {
+    public static void main(String[] args) {
+        new PoringConstructor();
+    }
+}
